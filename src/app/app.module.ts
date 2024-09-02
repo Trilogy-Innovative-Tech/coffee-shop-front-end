9cf8ab7e0f72cc7c82048411f0fadbd67480fcf4
Yasith Ishara Perera <yasithishara@gmail.com>
2024-09-02 13:00:10 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainUiComponent } from './main-ui/main-ui.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainUiComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
